b0b48d849c4820deac7e733e7332157123f6ec5d
Jaysonzhao <jaysonzhao@vip.qq.com>
2022-04-12 15:40:34 +0800

    Update Controller.java

--- src/main/java/com/learn/springbootkubernetes/Controller.java
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -2,11 +2,30 @@ package com.learn.springbootkubernetes;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @RestController
 public class Controller {
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring-Boot app successfully deployed and running on Minishift";
+        String shang = "中文测试";
+		String result = "";
+		System.out.println(shang);
+		try {
+			result = URLEncoder.encode(shang, "GBK");
+			System.out.println("GBK:" + result);
+			
+		    result = URLEncoder.encode(shang, "UTF-8");
+			System.out.println("UTF8: " + result);
+			
+			result = URLEncoder.encode(shang, "UTF-16");
+			System.out.println("UTF16:" + result);
+		} catch (UnsupportedEncodingException e) {
+			
+			e.printStackTrace();
+		}
+
+        return shang;
     }
 }
