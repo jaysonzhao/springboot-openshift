7451eb718a2c443a391233d82ba3bc85057e9b91
Jaysonzhao <jaysonzhao@vip.qq.com>
2022-11-24 16:57:55 +0800

    update sender

--- src/main/java/com/learn/springbootkubernetes/SpringbootKubernetesApplication.java
+++ src/main/java/com/learn/springbootkubernetes/SpringbootKubernetesApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import io.jaegertracing.Configuration;
 import io.jaegertracing.Configuration.ReporterConfiguration;
 import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.Configuration.SenderConfiguration;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -32,9 +33,12 @@ public class SpringbootKubernetesApplication {
 	}
 	@Bean
     public io.opentracing.Tracer initTracer() {
+		Configuration config = new io.jaegertracing.Configuration("java-trace-demo");
+        Configuration.SenderConfiguration sender = new Configuration.SenderConfiguration();
+        sender.withEndpoint("http://my-jaeger-collector:14268/api/traces"); 
         SamplerConfiguration samplerConfig = new SamplerConfiguration().withType("const").withParam(1);
         ReporterConfiguration reporterConfig = new ReporterConfiguration().withLogSpans(true);
-        return new Configuration("java-2-hello").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
+        return config.withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
     }
 
 }
