8b33592f5390b3821952d5ee616899780a9c24af
Jaysonzhao <jaysonzhao@vip.qq.com>
2022-11-24 15:33:04 +0800

    add tracer

--- src/main/java/com/learn/springbootkubernetes/SpringbootKubernetesApplication.java
+++ src/main/java/com/learn/springbootkubernetes/SpringbootKubernetesApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -28,5 +30,11 @@ public class SpringbootKubernetesApplication {
 
 		};
 	}
+	@Bean
+    public io.opentracing.Tracer initTracer() {
+        SamplerConfiguration samplerConfig = new SamplerConfiguration().withType("const").withParam(1);
+        ReporterConfiguration reporterConfig = new ReporterConfiguration().withLogSpans(true);
+        return new Configuration("java-2-hello").withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
+    }
 
 }
