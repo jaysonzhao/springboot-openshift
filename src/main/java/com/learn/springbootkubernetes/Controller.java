f2713b76e377303faa891aa2576a03c23c2dbe07
Jaysonzhao <jaysonzhao@vip.qq.com>
2022-11-24 15:20:39 +0800

    add span

--- src/main/java/com/learn/springbootkubernetes/Controller.java
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -1,18 +1,27 @@
 package com.learn.springbootkubernetes;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
 
 @RestController
 public class Controller {
+	@Autowired
+    private Tracer tracer;
+
     @RequestMapping("/")
     public String index() {
         String shang = "今天台风";
 		String result = "";
 		System.out.println(shang);
 		try {
+			 
 			result = URLEncoder.encode(shang, "GBK");
 			System.out.println("GBK:" + result);
 			
@@ -25,6 +34,16 @@ public class Controller {
 			
 			e.printStackTrace();
 		}
+		try (Scope scope = tracer.buildSpan("say-hello-handler").startActive(true)) {
+            String response = shang;
+			Span span = scope.span();
+            Map<String, String> fields = new LinkedHashMap<>();
+            fields.put("event", name);
+            fields.put("message", "this is a log message for name " + name);
+            span.log(fields);
+            span.setTag("response", response);
+           
+        }
 
         return shang;
     }
